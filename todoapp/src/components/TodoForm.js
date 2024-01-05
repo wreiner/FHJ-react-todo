4182aaa268ecebe137477363f3860f6807dbf42d
Walter Reiner <walter.reiner@wreiner.at>
2024-01-05 14:56:38 +0100

    Implement validation demo

--- todoapp/src/components/TodoForm.js
+++ todoapp/src/components/TodoForm.js
@@ -3,13 +3,25 @@ import './TodoForm.css';
 
 function TodoForm({list, listChanger, updateIdx, updateChanger}) {
   const [input, setInput] = useState('');
+  const [error, setError] = useState('');
 
   const onChangeValue = (e) => {
     setInput(e.target.value);
   };
 
+  const validateInput = () => {
+    if (input.length < 5) {
+      setError('lalla5');
+      return false;
+    }
+    return true;
+  };
+
   const onAddItem = () => {
     if (input.trim() !== '') {
+      if (!validateInput()) {
+        return;
+      }
       if (updateIdx !== null && updateIdx >= 0) {
         var array = [...list];
         array[updateIdx].todo = input;
@@ -33,7 +45,8 @@ function TodoForm({list, listChanger, updateIdx, updateChanger}) {
     <div>
       <form>
         <input value={input} onChange={onChangeValue} placeholder='What do you need todo?' className='inputclass' />
-        <button type="button" onClick={onAddItem}>
+        {error && <p style={{ color: 'red', margin: '0', alignSelf: 'flex-start' }}>{error}</p>}
+        <button type="button" onClick={onAddItem} style={{ alignSelf: 'flex-start', marginTop: '10px' }}>
           {updateIdx !== null ? "Update" : "Add"} Item
         </button>
       </form>
