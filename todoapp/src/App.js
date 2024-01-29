359e26b4a8fa3d1955d874c32c0033e72c21f4d9
Walter Reiner <walter.reiner@wreiner.at>
2024-01-29 19:49:13 +0100

    Add localstorage

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
-import { useState } from 'react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
   const [list, setList] = useState([]);
-  // const [updateEntry, setUpdateEntry] = useState({});
+
+  useEffect(() => {
+    console.log("in useEffect");
+    const storedList = localStorage.getItem('todoList');
+    console.log(storedList);
+    if (storedList !== "undefined" && storedList !== null) {
+
+      setList(JSON.parse(storedList));
+    }
+  }, []);
+
+  const handleListChange = (newList) => {
+    if (typeof newList === 'function') {
+      setList((prevList) => {
+        const updatedList = newList(prevList);
+        localStorage.setItem('todoList', JSON.stringify(updatedList));
+        return updatedList;
+      });
+    } else {
+      setList(newList);
+      localStorage.setItem('todoList', JSON.stringify(newList));
+    }
+  };
 
   return (
     <>
       <h1>ToDo List App</h1>
 
-      <TodoForm list={list} listChanger={setList} updateIdx={null} updateChanger={null} />
+      <TodoForm list={list} listChanger={handleListChange} updateIdx={null} updateChanger={null} />
 
-      <TodoList list={list} listChanger={setList} />
+      <TodoList list={list} listChanger={handleListChange} />
 
     </>
   );
